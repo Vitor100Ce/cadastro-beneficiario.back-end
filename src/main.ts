a23b43e3fea2c6c88b25a2f12dc649fe529e5144
Vitor100Ce <88120850+Vitor100Ce@users.noreply.github.com>
2023-03-08 20:42:29 -0300

    configurado swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Cadastro de beneficiários')
+    .setDescription('Sistemas para cadastro de beneficários e dependentes de um plano de saúde')
+    .setVersion('1.0')
+    .addTag('Cadastro de beneficiários')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
